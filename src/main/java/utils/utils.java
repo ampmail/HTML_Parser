fb20744bf4b5e78868d48c831c78e9b1f2fb7227
ampmail <bounty.mail@gmail.com>
2015-09-01 19:17:37 +0300

    update2

---
+++ src/main/java/utils/utils.java
@@ -0,0 +1,20 @@
+package utils;
+
+/**
+ * Created by AMP on 31.08.2015.
+ */
+public class utils {
+    public static String MD5(String md5) {
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            byte[] array = md.digest(md5.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return null;
+    }
+}
